ebaa1835a72b951fe4032734382f366412f8cf43
richeek45 <richeek45@gmail.com>
2024-07-15 20:58:55 +0530

    adding shadcn and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import logo from "./svg/logo.svg";
+import call from "./svg/call.svg";
 
 function App() {
 
   return (
-    <div>
-      Start 
+    <div> 
+      <nav className='flex justify-between p-2'>
+        <img src={logo} alt="logo" />
+        <div className='border-yellow-400	border-2 rounded-md p-2 drop-shadow-md'>
+          <img src={call} alt="call" />
+        </div>
+      </nav>
+
+
+
     </div>
   )
 }
