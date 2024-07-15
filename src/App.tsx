b0dafd9d5eaa5995d164dbc6fa83d88f4fec3d3b
richeek45 <richeek45@gmail.com>
2024-07-15 22:47:24 +0530

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import logo from "./svg/logo.svg";
 import call from "./svg/call.svg";
+import backgroundDesign from "./assets/hero.png";
+import coworking from "./assets/Coworking.png";
 
 function App() {
 
@@ -14,9 +16,18 @@ function App() {
       </nav>
 
 
+      <div className='relative flex lg:justify-between justify-center'>
+        <p className=' text-left text-5xl font-bold w-[50%] hidden lg:block my-auto mx-auto'>
+          Host your meeting with world-class amenities. <p>Starting at <span className='text-yellow-400	'>₹199/-!</span></p>
+        </p>
+        <img src={coworking} alt='coworking' />
+        <div className='absolute top-0 left-0 hidden lg:block'>
+          <img src={backgroundDesign} alt='background-image' />
+        </div>
+      </div>
 
     </div>
   )
 }
 
-export default App
+export default App;
